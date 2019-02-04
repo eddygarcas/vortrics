47e9ec9016dba930c33d563543a66cbbda378ef2
eduard.garcia <edugarcas@gmail.com>
2019-02-04 07:34:33 +0100

    - Quick fix to get change logs that a like another string key. - Performance improvement, all data from graph calls will be stored in Redis - Naming convention, external service helper methods - Adding legends in Release graph to improve its usability - Average release time included on the graph - Sprint Scope Change panel - Cosmetic changes - Fixing Redis sprint data stored - Change log level in production - Added average bugs per sprint as part of the team summary - Fixes on Sprint summary dashboard - Added a open vs closed graph bugs form the last 3 months - Fixing Release time and Backlog time on the issue summary screen - Refactoring the average graph to see its evolutions along time - Redesign dashboard screen, now the comulative flow diagram has its own place, left side of the release time bugs. - Added a User & Groups management

---
+++ app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,9 @@
+class UserNotifierMailer < ApplicationMailer
+	default from: 'info@voardtrix.com'
+
+	def send_info_email(user)
+		@user = user
+		mail(to: 'edugarcas@gmail.com',
+		     subject: "Grant access to #{user.email}")
+	end
+end
