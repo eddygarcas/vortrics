3d155d0b6a6b43b47f9ee4e9ebe0f639a00d7c1d
eduard.garcia <edugarcas@gmail.com>
2019-02-18 10:19:10 +0100

    - Quick fix to get change logs that a like another string key. - Performance improvement, all data from graph calls will be stored in Redis - Naming convention, external service helper methods - Adding legends in Release graph to improve its usability - Average release time included on the graph - Sprint Scope Change panel - Cosmetic changes - Fixing Redis sprint data stored - Change log level in production - Added average bugs per sprint as part of the team summary - Fixes on Sprint summary dashboard - Added a open vs closed graph bugs form the last 3 months - Fixing Release time and Backlog time on the issue summary screen - Refactoring the average graph to see its evolutions along time - Redesign dashboard screen, now the comulative flow diagram has its own place, left side of the release time bugs. - Added a User & Groups management - Add rest call to get field information - Refactor external service information form to be used in other screens. - Workflow screen added - Create a new user wizard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable, :registerable
-	devise :database_authenticatable, :registerable,
+	devise :database_authenticatable,
 	       :recoverable, :rememberable, :trackable, :validatable
 
 	has_one :access, dependent: :destroy
