e7cd3e6a5586ca324b986606235edcdf2ca2eed7
Eduard Garcia <edugarcas@gmail.com>
2019-05-03 15:06:51 +0200

    - Upgrade devise to version 4.6.0 or later - Refactoring Comulative Flow Diagram logic - Remove static project name and icon definition - Refactoring parsing jira issue - Fixing display actual user team when loading vortrics - Add Advice screens - Advice RSpec testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,10 +27,6 @@ class ApplicationController < ActionController::Base
 
 	protected
 
-	def side_nav_info
-		@total_teams = Team.all.count
-	end
-
 	def sprint_by_board board_id, sort_column, sort_direction, options = {}
 		return if board_id.blank?
 		@board_sprint = boards_by_sprint board_id, 0, options
@@ -62,10 +58,17 @@ class ApplicationController < ActionController::Base
 			@team = Team.find(id)
 		end
 		session[:team_id] = @team.id unless @team.blank?
-		side_nav_info
+		advice_messages
 	end
 
 	private
+
+	def advice_messages
+		return if @team.blank?
+		ScrumMetrics.config[:advices].each_key do |key|
+			@team.advices.where(advice_type: key.to_s).first_or_create(ScrumMetrics.config[:advices][key]) if @team.send(key)
+		end
+	end
 
 	def get_jira_client
 		return if current_user.setting.blank?
