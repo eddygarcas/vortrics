7275e1d204cecaceb864e56dc59733776164dab4
duard <edugarcas@gmail.com>
2019-07-16 23:42:36 +0200

    - fixing schema file

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,4 +5,9 @@ class Card < ApplicationRecord
   scope :by_list_name, -> (name) {joins(:workflow).where({workflows: {name: name}})}
 
   validates :name, presence: true
+  before_save :name_downcase
+
+  def name_downcase
+    self.name.downcase!
+  end
 end
