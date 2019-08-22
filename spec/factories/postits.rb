ce427917bbfdcbf90f99188de23f744c5ba1104a
Eduard Garcia <edugarcas@gmail.com>
2019-08-22 14:10:32 +0200

    - Upgrade devise to version 4.6.0 or later - Refactoring Comulative Flow Diagram logic - Remove static project name and icon definition - Refactoring parsing jira issue - Fixing display actual user team when loading vortrics - Add Advice screens - Advice RSpec testing - Remove a call that application controller was making to get user data from JIRA. - Add a logic inside user controller to get user information form jira and store it just once. - Add comments section - Adding a proper notification system for comments, this will allow us to comunicate with our colleagues in order to improve the team process. - Clean up files - Adding ActiveRecord scope functionality into models - Refactoring Javascript to be coffeescript - Import kanban issues logic - Refactorig rickshaw graphics - Refactoring the site to accept Teams using kanban - Refactoring Kanban teams - Functionality to be able to add new advices. - Refactoring workflow functionality - Refactoring workflow functionality adding cards and replace the status search functionality. - Working on retrospective functionality

--- spec/factories/postits.rb
+++ spec/factories/postits.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :postit do
     text { Faker::Name.female_first_name }
-    position { Faker::Number.between(1,200) }
-    dots { Faker::Number.between(1,4) }
-    comments { Faker::Number.between(1,4) }
+    position { Faker::Number.between }
+    dots { Faker::Number.between }
+    comments { Faker::Number.between }
     retrospective { FactoryBot.create(:retrospective) }
     user {FactoryBot.create(:user)}
   end
--- spec/factories/project_infos.rb
+++ spec/factories/project_infos.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :project_info do
-    key { Faker::Alphanumeric.alpha 4 }
+    key { Faker::Alphanumeric.alpha }
     name { Faker::Name.suffix }
     icon { Faker::Avatar.image }
   end
