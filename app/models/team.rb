c60026639453216a82b4f67269c5f21596eb396d
Eduard Garcia <edugarcas@gmail.com>
2020-01-04 11:26:07 +0100

    Be able to delete issues as part of the clean up section.

--- app/models/team.rb
+++ app/models/team.rb
@@ -59,7 +59,7 @@ class Team < ApplicationRecord
 
   def issues_selectable_for_graph
     Rails.cache.fetch("issues_selectable_for_graph_#{id}", expires_in: 1.day) {
-      #limit = ScrumMetrics.config[:performance][:graph_limit].to_i
+      #limit = Vortrics.config[:performance][:graph_limit].to_i
       Issue.joins(:sprint).where('team_id = ?', id).select('issues.*').select(&:selectable_for_cycle_time?).sort_by!(&:cycle_time)
     }
   end
