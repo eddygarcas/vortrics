73d4cbed85d6081356936525b8305e8f74369080
Eduard Garcia <edugarcas@gmail.com>
2020-02-26 09:13:02 +0100

    Setting will accept no SSL for local Jira instances

--- app/javascript/components/commentpanel.vue
+++ app/javascript/components/commentpanel.vue
@@ -1,45 +1,60 @@
 <template>
-    <div v-model="comments" class="tab-pane fade active in" style='height: 275px; overflow: scroll;'>
-        <ul class="list-group list-group-flush">
-            <li class='list-group-item'  v-for="(comment, index) in comments">
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img height="24" width="24" class="img-rounded" v-tooltip:right="comment.first_time.author.displayName"
-                             v-bind:src="comment.first_time.author.avatarUrls['48x48']">
-                    </a>
-                    <div class="media-body">
-                        <h5 class="media-heading small">
-                            <img height="18" width="18" v-bind:src="comment.priority.icon" v-tooltip:right="comment.priority.name">
-                            <a v-bind:href="'/issues/key/' + comment.key">{{comment.key}}</a> Time until first comment
-                            {{ comment.first_time.created, comment.created | toHours }} hours
-                            <img height="18" width="18" v-bind:src="comment.status.icon" v-tooltip:bottom="comment.status.name">
-                        </h5>
-                        <p class='small'>{{comment.first_time.body}}</p>
-                    </div>
-                </div>
-            </li>
-        </ul>
+    <div class="panel panel-info" style="height: 335px;" :key="componentKey">
+        <div class="panel-heading">
+            <label class="switch switch-info pull-right font_small"
+                   v-tooltip:bottom="'First Time Response as hours or days'"> HOURS/DAYS
+                <input type="checkbox" id="first_response_switch" v-on:change="refresh" v-model="isdays">
+                <span style="margin-bottom: -5px;"></span>
+            </label>
+            <p class='lead'><i class="fa fa-fw fa-comments fa-lg fa-fw"></i> FIRST COMMENTS</p>
+        </div>
+
+        <div class="tab-content">
+            <div v-model="comments" class="tab-pane fade active in" style='height: 275px; overflow: scroll;'>
+                <ul class="list-group list-group-flush">
+                    <li class='list-group-item' v-for="(comment, index) in comments">
+                        <div class="media">
+                            <a class="pull-left" href="#">
+                                <img height="24" width="24" class="img-rounded"
+                                     v-tooltip:right="comment.first_time.author.displayName"
+                                     v-bind:src="comment.first_time.author.avatarUrls['48x48']">
+                            </a>
+                            <div class="media-body">
+                                <h5 class="media-heading small">
+                                    <img height="18" width="18" v-bind:src="comment.priority.icon"
+                                         v-tooltip:right="comment.priority.name">
+                                    <a v-bind:href="'/issues/key/' + comment.key">{{comment.key}}</a> Time until first
+                                    comment
+                                    {{ comment.first_time.created, comment.created, timein | toHours }} {{timein}}
+                                    <img height="18" width="18" v-bind:src="comment.status.icon"
+                                         v-tooltip:bottom="comment.status.name">
+                                </h5>
+                                <p class='small'>{{comment.first_time.body}}</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import moment from 'moment'
 
-
-
     export default {
         components: {},
         filters: {
-            toHours: function (start,stop) {
+            toHours: function (start, stop, timein) {
                 if (start && stop) {
-                    return moment(String(start)).diff(stop,"hours")
+                    return moment(String(start)).diff(stop, timein)
                 }
             }
         },
         props: [],
         computed: {
             comments: {
-                get(){
+                get() {
                     return this.$store.state.comments.list;
                 }
             },
@@ -51,7 +66,11 @@
 
         },
         data: function () {
-            return {}
+            return {
+                timein: "hours",
+                isdays: false,
+                componentKey: 0
+            }
         },
         created: function () {
             $.ajax({
@@ -61,11 +80,20 @@
                 processData: false,
                 contentType: false,
                 success: (data) => {
-                    this.$store.commit('byBoardComments',data);
+                    this.$store.commit('byBoardComments', data);
                 }
             })
         },
-        methods: {}
+        methods: {
+            refresh: function () {
+                if (this.isdays) {
+                    this.timein = "days"
+                } else {
+                    this.timein = "hours"
+                }
+                this.componentKey += 1
+            }
+        }
     }
 </script>
 
