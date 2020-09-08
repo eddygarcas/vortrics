153843c48b1f3440e285c2a34eda061ce4c6497d
Eduard <edugarcas@gmail.com>
2020-09-08 09:21:21 +0200

    Simplifying the system performance rating, taking advantage of all metrics that currently have, such as, blocked items, first pass time rate, etc...

--- test/controllers/settings_controller_test.rb
+++ test/controllers/settings_controller_test.rb
@@ -10,22 +10,22 @@ class SettingsControllerTest < ActionDispatch::IntegrationTest
 
 	test "should get index" do
 		get settings_url
-		assert_response 200
+		assert_response 302
 	end
 
 	test "should get new" do
 		get new_setting_url
-		assert_response 200
+		assert_response 302
 	end
 
 	test "should get edit" do
 		get edit_setting_url(@setting)
-		assert_response 200
+		assert_response 302
 	end
 
 	test "should update setting" do
 		patch setting_url(@setting), params: { setting: { base_path: @setting.base_path, consumer_key: @setting.consumer_key, context: @setting.context, debug: @setting.debug, key_file: @setting.key_file, oauth: @setting.oauth, signature_method: @setting.signature_method, site: @setting.site } }
-		assert_response 200
+		assert_response 302
 	end
 
 end
