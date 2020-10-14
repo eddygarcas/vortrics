7c98320787783f5041e1d100d01114ec04eb2154
Eduard Garcia <edugarcas@gmail.com>
2020-10-14 17:39:47 +0200

    Setting will accept no SSL for local Jira instances

--- app/controllers/jira_sessions_controller.rb
+++ app/controllers/jira_sessions_controller.rb
@@ -37,7 +37,6 @@ class JiraSessionsController < ApplicationController
 
   def get_jira_client
     return if current_user&.setting.blank?
-    pp "Session data #{session}"
     update_jira_tokens
     @jira_client = Jira::Client.instance(current_user)
     flash[:danger] = Vortrics.config[:messages][:external_service_error] if @jira_client.nil?
